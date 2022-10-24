d6ec5fc296c4ffa6f0019af6dd505c9817a0e572
Cantor Alexandru <alexcantor64@gmail.com>
2022-10-24 19:30:58 +0300

    Modify labs

--- Lab 1/lab1.pl
+++ Lab 1/lab1.pl
@@ -1,3 +1,4 @@
+% Calculate a factorial for a natural number
 factorial(0,1).
 factorial(N,M) :-
 N>0,
@@ -5,6 +6,11 @@ N1 is N-1,
 factorial(N1, M1),
 M is N*M1.
 
+
+% Write a predicate to determine the 
+% lowest common multiple of a list formed from integer numbers.
+% lcm2(A-Value,integer, B-Value,integer, M-result,integer)
+% flow model: lcm2(i,i,o)
 lcm2(A,B,M) :-
 0 is A mod B ,
 M is A.
@@ -13,6 +19,8 @@ lcm2(A,B,M) :-
 lcm2(B,A mod B, MT),
 M is (MT*A)/(A mod B).
 
+% lcm(A-List, M-result,integer)
+% flow model: lcm(i,o)
 lcm([],_).
 lcm([A],M) :-
 length([A],N),
--- Lab 2/Lab2.pl
+++ Lab 2/Lab2.pl
@@ -1,5 +1,7 @@
 % Write a predicate to add a value v 
 % after 1-st, 2-nd, 4-th, 8-th, ... element in a list.
+% addValue(L-list, V-value, integer, M-result, list)
+% flow model: addValue(i,i,o)
 
 addValue([],_,[]).
 
@@ -7,6 +9,9 @@ addValue(H,V,M) :-
     length(H,L),
 	add(H,1,V,L,M).
 
+
+% addValue(L-list,K-value, integer V-value, integer,L-value,integer M-result, list)
+% flow model: addValue(i,i,i,i,o)
 add([],_,_,_,[]).
 
 add([H],K,_,N,[H]) :- K > N.
@@ -20,5 +25,3 @@ add([H|T],K,V,N,[H|MT]) :-
     K=<N,
     0 =\= K /\(K-1),
 	add(T,K+1,V,N,MT).
-    
-    
